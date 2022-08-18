d409e1a9827860080fde1bf6d9dd8b291581ad20
pc <ddooochii@gmail.com>
2022-08-18 15:22:32 +0900

    commit

--- src/main/java/com/Imadeveloperrr/book/springboot/web/ProfileController.java
+++ src/main/java/com/Imadeveloperrr/book/springboot/web/ProfileController.java
@@ -1,5 +1,4 @@
 package com.Imadeveloperrr.book.springboot.web;
-
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,6 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
 import java.util.List;
+
 
 @RequiredArgsConstructor
 @RestController
@@ -17,7 +17,7 @@ public class ProfileController {
     public String profile() {
         List<String> profiles = Arrays.asList(env.getActiveProfiles());
         List<String> realProfiles = Arrays.asList("real", "real1", "real2");
-        String defaultProfile = profiles.isEmpty() ? "default" : profiles.get(0);
+        String defaultProfile = profiles.isEmpty()? "default" : profiles.get(0);
 
         return profiles.stream()
                 .filter(realProfiles::contains)
